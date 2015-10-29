318204d6fee1d9b4e968beff432610c09da0a40a
Fabien Benureau <fabien.benureau+git@gmail.com>
2015-10-29 22:02:28 +0100

    versioneer support

---
+++ clusterjobs/__init__.py
@@ -0,0 +1,6 @@
+
+from ._version import get_versions
+__version__ = get_versions()["version"]
+__commit__ = get_versions()["full-revisionid"]
+__dirty__ = get_versions()["dirty"]
+del get_versions
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import os
 from distutils.core import setup
 
+import versioneer
+
 setup(
-    name = "clusterjobs",
-    version = "0.0_alpha2",
-    author = "Fabien Benureau",
+    name         = "clusterjobs",
+    version      = "1.0",
+    cmdclass     = versioneer.get_cmdclass()
+    author       = "Fabien Benureau",
     author_email = "fabien.benureau@inria.fr",
-    description = ("A python library for creating, scheduling and monitoring cluster jobs"),
-    license = "Not Yet Decided.",
-    keywords = "clusters",
-    url = "flowers.inria.fr",
-    packages=['clusterjobs'],
+    description  = "A python library for creating, scheduling and monitoring cluster jobs",
+    license      = "Open Science License",
+    keywords     = "clusters science",
+    url          = "github.com/humm/clusterjobs",
+    packages     = ['clusterjobs'],
     #long_description=read('README'),
-    classifiers=[],
+    classifiers = [],
 )
